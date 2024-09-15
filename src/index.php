9dd1fda97089e4b12592cd5f87929a1b0970bab7
Mateus <ext.mateus@icloud.com>
2024-09-15 15:41:05 -0300

    Checando o diretorio correta com is_writable

--- src/index.php
+++ src/index.php
@@ -1,17 +1,18 @@
 <?php
 
-$dir = getcwd() . '/testdir';
+$dir = getcwd();
+$new_dir = $dir  . '/testdir';
 
 echo 'Running as user: ' . get_current_user() . "<br>";
 
-if (!is_writable('/usr/share/nginx/html')) {
+if (!is_writable($dir)) {
     echo "The directory $dir is not writable.<br>";
 } else {
     echo "The directory $dir is writable.<br>";
 }
 
 if (!is_dir($dir)) {
-    if (mkdir($dir, 0755, true)) {
+    if (mkdir($new_dir, 0755, true)) {
         echo "Directory created successfully.";
     } else {
         $error = error_get_last();
